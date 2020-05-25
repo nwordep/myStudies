8ce71787f3b641ceca19b18604877c60a1b7b4fd
nwordep <65229002+nwordep@users.noreply.github.com>
2020-05-25 18:26:38 -0300

    Update ideia.c
    um dia volto no c...

--- C/Lista de Compras/ideia.c
+++ C/Lista de Compras/ideia.c
@@ -1,29 +1,16 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main(){
+int main(void){
 
-     char item_1;
-     char item_2; 
+char item_1;
+char item_2; 
 
     setlocale (LC_ALL, "Portuguese");
-
-    printf ("\nInsira um único item por vez, ao terminar\n");
+    printf ("\nInsira um único item por vez\n");
     
     scanf  ("%c", &item_1);
-    printf ("Item %c adicionado, insira o próximo item: ", item_1);
+    printf ("Item %c adicionado, insira o próximo item: \n", item_1);
     scanf  ("%c",item_2);
-    printf ("Item %c adicionado. )
-    
-    return; 
-    
-
-
-   
-    
-    
-    
-    
-                                                                                   //inicio declaração variaveis da lista
-                                                                            
-}
+    printf ("Item %c adicionado.");
+    }                                                                                                                                                              
